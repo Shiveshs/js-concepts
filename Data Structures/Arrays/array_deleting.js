ce72636d57839d5f67b90a9c74128465e5a815e3
Shivesh Shandilya <shivesh.shandilya@gmail.com>
2024-01-09 16:02:39 +0530

    Added conditions

--- Data Structures/Arrays/array_deleting.js
+++ Data Structures/Arrays/array_deleting.js
@@ -1,28 +1,24 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var data = [1, 2, 4, 6, 77, 4, 3, 3, 45];
-var deleteInput = document.getElementById("deleteInput");
-var result = document.getElementById("resultList");
-var list;
-var deleteArrayHandler = function () {
-    if (list) {
-        result.removeChild(list);
+const data = [1, 2, 4, 6, 77, 4, 3, 3, 45];
+let deleteInput = document.getElementById("deleteInput");
+let result = document.getElementById("resultList");
+let list;
+let deleteArrayHandler = function () {
+  let deleteInputValue = (deleteInput.value);
+  console.log(deleteInputValue)
+  if (deleteInputValue !== undefined && deleteInputValue !== null &&  deleteInputValue !== "" && !isNaN(Number(deleteInputValue)) && data.indexOf(Number(deleteInputValue)) !== -1  ) {
+    for (let i = data.indexOf(Number(deleteInputValue)); i < data.length; i++) {
+      data[i] = data[i + 1];
     }
-    var deleteInputValue = Number(deleteInput.value);
-    for (var i = data.indexOf(deleteInputValue); i < data.length; i++) {
-        if (deleteInputValue !== undefined ||
-            deleteInputValue !== null) {
-            data[i] = data[i + 1];
-        }
-        else {
-            alert("provide a valid value from above array");
-        }
-    }
-    data.length = data.length - 1;
-    for (var i = 0; i < data.length; i++) {
-        list = document.createElement("li");
-        list.innerText = data[i];
-        result.appendChild(list);
-    }
-    // deleteInput.value = undefined; Write a statement to reset the input value
+  
+  data.length = data.length - 1;
+  for (let i = 0; i < data.length; i++) {
+    list = document.createElement("li");
+    list.innerText = data[i];
+    result.appendChild(list);
+  }
+}
+  else {
+    alert("provide a valid value from above array");
+  }
+  // deleteInput.value = undefined; Write a statement to reset the input value
 };
